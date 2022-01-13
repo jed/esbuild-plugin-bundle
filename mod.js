70d934ae85203487ebb0689af2a3a7e6346c1e69
Jed Schmidt <where@jed.is>
2022-01-12 22:44:10 -0500

    Update mod.js

--- mod.js
+++ mod.js
@@ -13,19 +13,13 @@ export default {
         entryPoints: [path],
         bundle: true,
         write: false,
-        metafile: true
+        metafile: true,
+        outdir: '/'
       })
 
-      let sourceObj = outputFiles.find(x => x.path.endsWith('.js'))
-      let mapObj = outputFiles.find(x => x.path.endsWith('.js.map')) || {}
-      let buffer = new TextEncoder().encode(sourceObj.contents)
-      let digest = await crypto.subtle.digest('SHA-1', buffer)
-      let arr = [...new Uint8Array(digest)].slice(0, 4)
-      let hash = arr.map(i => i.toString(16).padStart(2, '0')).join('')
-      let source = sourceObj.text
-      let map = mapObj ? mapObj.text : undefined
-      let contents = JSON.stringify({source, map, hash})
       let watchFiles = Object.keys(metafile.inputs)
+      let entries = outputFiles.map(x => [x.path, x.text])
+      let contents = JSON.stringify(Object.fromEntries(entries))
       return {loader: 'json', contents, watchFiles}
     })
   }
